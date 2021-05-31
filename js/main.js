49b30b82b122f5a5fa03b7911e402b5979a5daee
Гульназ <gulnazbackirova@yandex.ru>
2021-05-31 19:03:30 +0500

    Функции правки

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 function getRandomInRange(min, max) {
   if (max <= min) {
-    console.log ('Слишком маленькое число!');
+    return 'Второе число не может быть меньше первого!';
+  }
+  if (min < 0 || max < 0) {
+    return 'Нельзя использовать отрицательное число!';
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+}
+getRandomInRange(0, 3)
 
 
 function getRandomFloat(min, max, num) {
   if (max <= min) {
-    console.log ('Слишком маленькое число!');
+    return 'Второе число не может быть меньше первого!';
   }
-  console.log (Math.random() * (max - min) + min, (Math.random() * (max - min) + min).toFixed(num));
-};
+  if (min < 0 || max < 0) {
+    return 'Нельзя использовать отрицательное число!';
+  }
+  return (Math.random() * (max - min) + min).toFixed(num);
+}
+getRandomFloat(1, 3, 3)
 
