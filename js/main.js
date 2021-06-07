8aa3898260584af00c0ccd888a8eb13f8e9ca1d4
Гульназ <gulnazbackirova@yandex.ru>
2021-06-07 19:53:38 +0500

    Задание 8: вторая попытка

--- js/main.js
+++ js/main.js
@@ -21,34 +21,55 @@
 //}
 //getRandomFloat(1, 3, 3)
 
+const getRandomIntegerInRange = (elements) => {
+  return elements[_.random(0, elements.length - 1)];
+};
+const FEATURES = [wifi, dishwasher, parking, washer, elevator, conditioner];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const CHECKIN = ['12:00','13:00','14:00'];
+const CHECKOUT = ['12:00','13:00','14:00'];
+const TITLE = ['Квартира', 'Отель', 'Комната', 'Кемпинг', 'Трейлер'];
+
+function price (min, max) {
+  const MAX_PRICE = 10000;
+    if (min < 0 || max < 0) {
+    return 'Цена не может быть меньше 0!';
+     }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+function rooms (min, max) {
+  const MAX_ROOMS = 10000;
+    if (min < 0 || max < 0) {
+    return 'Количество комнат не может быть меньше 0!';
+     }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+function guests (min, max) {
+  const MAX_GUESTS = 10000;
+    if (min < 0 || max < 0) {
+    return 'Количество гостей не может быть меньше 0!';
+     }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
 let author = {
   avatar: function () {
     let i = Math.round(Math.random() * (8 - 1) + 1);
-    let link = "img/avatars/user0" + i + ".png";
+    let link = 'img/avatars/user0' + i + '.png';
     return link;
   }
 
 };
 
 
-
-const features = [wifi, dishwasher, parking, washer, elevator, conditioner];
-const photos = [https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg,
-https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg,
-https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg];
-
 let offer = {
-  title: 'Заголовок',
+  title,
 
   address: location.x, location.y,
 
-  price: function (min, max) {
-    if (min < 0 || max < 0) {
-    return 'Цена не может быть меньше 0!';
-     }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-  },
+  getRandomIntegerInRange(0, MAX_PRICE),
 
   type: {
     palace,
@@ -58,19 +79,9 @@ let offer = {
     hotel
   },
 
-  rooms: function(min, max) {
-    if (min < 0 || max < 0) {
-    return 'Количество комнат не может быть меньше 0!';
-     }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-  },
+  getRandomIntegerInRange(0, MAX_ROOMS),
 
-  guests: function (min, max) {
-    if (min < 0 || max < 0) {
-    return 'Количество гостей не может быть меньше 0!';
-     }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-  },
+  getRandomIntegerInRange(0, MAX_GUESTS),
 
   checkin: {
   '12:00',
@@ -82,29 +93,16 @@ let offer = {
   '13:00',
   '14:00'},
 
-  getFeatures: function () {
-    Math.floor(Math.random() * features.length);
-  },
+  features: [],
 
-  description: 'Описание',
+  description: 'Расскажите подробнее о вашем жилье',
 
-  getPhotos: function () {
-    Math.floor(Math.random() * photos.length);
-  }
+  photos: []
 };
 
 let location = {
-  lat: function(min, max) {
-  min = 35.65000;
-  max = 35.70000;
-  return Math.random() * (max - min) + min;
-  },
-
-  lng: function(min, max) {
-  min = 139.70000;
-  max = 139.80000;
-  return Math.random() * (max - min) + min;
-  }
+  lat: 0,
+  lng: 0
 };
 
 
