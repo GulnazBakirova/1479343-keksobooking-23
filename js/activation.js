29f391eaa86804bfb0d5b06b2452ac765c01fb6c
Гульназ <gulnazbackirova@yandex.ru>
2021-06-24 19:43:32 +0500

    7 раздел 2 задание №2

--- js/activation.js
+++ js/activation.js
@@ -1,14 +1,16 @@
 const adForm = document.querySelector('.ad-form');
 const adFormFieldsets = document.querySelectorAll('.ad-form__element');
+const mapForm = document.querySelector('.map__filters');
+const mapFilters = mapForm.querySelector('.map__filter');
 
-const activateForm = function () {
+const activateAdForm = function () {
   adForm.classList.remove('ad-form--disabled');
   adFormFieldsets.forEach((element) => {
     element.disabled = false;
   });
 };
 
-const deactivateForm = function () {
+const deactivateAdForm = function () {
   adForm.reset();
   adFormFieldsets.forEach((element) => {
     element.disabled = true;
@@ -16,4 +18,20 @@ const deactivateForm = function () {
   adForm.classList.add('ad-form--disabled');
 };
 
-deactivateForm();
+const activateMapForm = function () {
+  mapForm.classList.remove('map-form--disabled');
+  mapFilters.forEach((element) => {
+    element.disabled = false;
+  });
+};
+
+const deactivateMapForm = function () {
+  mapForm.reset();
+  mapFilters.forEach((element) => {
+    element.disabled = true;
+  });
+  mapForm.classList.add('map-form--disabled');
+};
+
+deactivateAdForm();
+deactivateMapForm();
