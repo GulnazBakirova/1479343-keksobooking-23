051648b1cdc62778389be586272d7d81a51c9d10
Гульназ <gulnazbackirova@yandex.ru>
2021-06-25 15:40:33 +0500

    Раздел 8, задание 1, №1

---
+++ js/activation.js
@@ -0,0 +1,37 @@
+const adForm = document.querySelector('.ad-form');
+const adFormFieldsets = document.querySelectorAll('.ad-form__element');
+const mapForm = document.querySelector('.map__filters');
+const mapFilters = mapForm.querySelectorAll('.map__filter');
+
+const activateAdForm = function () {
+  adForm.classList.remove('ad-form--disabled');
+  adFormFieldsets.forEach((element) => {
+    element.disabled = false;
+  });
+};
+
+const deactivateAdForm = function () {
+  adForm.reset();
+  adFormFieldsets.forEach((element) => {
+    element.disabled = true;
+  });
+  adForm.classList.add('ad-form--disabled');
+};
+
+const activateMapForm = function () {
+  mapForm.classList.remove('map-form--disabled');
+  mapFilters.forEach((element) => {
+    element.disabled = false;
+  });
+};
+
+const deactivateMapForm = function () {
+  mapForm.reset();
+  mapFilters.forEach((element) => {
+    element.disabled = true;
+  });
+  mapForm.classList.add('map-form--disabled');
+};
+
+deactivateAdForm();
+deactivateMapForm();
